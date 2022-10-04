583f71ec0c8b96e345de02bb682e11ea67601186
Pugazhprince <89248877+Pugazhprince@users.noreply.github.com>
2022-10-05 00:40:04 +0530

    Finished User Authentication Functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,98 +7,90 @@ import { createCustomAPIError } from "../errors/custom-error.js";
 //Desc Register New User
 //Route /api/users (POST)
 //Access Public
-const registerUser = async (req, res, next) => {
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    return next(
-      createCustomAPIError("Please Add All Fields", StatusCodes.BAD_REQUEST)
-    );
-  }
-  //Check User Exists
-  const userExists = await User.findOne({ email });
+const registerUser = async(req, res, next) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        return next(
+            createCustomAPIError("Please Add All Fields", StatusCodes.BAD_REQUEST)
+        );
+    }
+    //Check User Exists
+    const userExists = await User.findOne({ email });
 
-  if (userExists) {
-    return next(
-      createCustomAPIError("User Already Exists", StatusCodes.BAD_REQUEST)
-    );
-  }
+    if (userExists) {
+        return next(
+            createCustomAPIError("User Already Exists", StatusCodes.BAD_REQUEST)
+        );
+    }
 
-  //Hash Password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
+    //Hash Password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-  //Create User
-  const user = await User.create({
-    name,
-    email,
-    password: hashedPassword,
-  });
-  const payload = {
-    id: user._id,
-    name: user.name,
-  };
-
-  if (user) {
-    res.status(StatusCodes.CREATED).json({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(payload),
+    //Create User
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
     });
-  } else {
-    return next(
-      createCustomAPIError("Invalid User Data", StatusCodes.BAD_REQUEST)
-    );
-  }
+    const payload = {
+        id: user._id,
+        name: user.name,
+    };
+
+    if (user) {
+        res.status(StatusCodes.CREATED).json({
+            token: generateToken(payload),
+        });
+    } else {
+        return next(
+            createCustomAPIError("Invalid User Data", StatusCodes.BAD_REQUEST)
+        );
+    }
 };
 
 //Desc Authenticate New User
 //Route /api/users/login (POST)
 //Access Public
-const loginUser = async (req, res, next) => {
-  const { email, password } = req.body;
-  //Check for User Email
-  const user = await User.findOne({ email });
-  const payload = {
-    id: user._id,
-    name: user.name,
-  };
+const loginUser = async(req, res, next) => {
+    const { email, password } = req.body;
+    //Check for User Email
+    const user = await User.findOne({ email });
+    const payload = {
+        id: user._id,
+        name: user.name,
+    };
 
-  if (user && (await bcrypt.compare(password, user.password))) {
-    res.status(StatusCodes.OK).json({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(payload),
-    });
-  } else {
-    return next(
-      createCustomAPIError("Invalid Credentials", StatusCodes.BAD_REQUEST)
-    );
-  }
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.status(StatusCodes.OK).json({
+            token: generateToken(payload),
+        });
+    } else {
+        return next(
+            createCustomAPIError("Invalid Credentials", StatusCodes.BAD_REQUEST)
+        );
+    }
 };
 //Desc   GET User data
 //Route /api/users/me (GET)
 //Access Private
-const getMe = async (req, res) => {
-  const { _id, name, email } = await User.findById(req.user.id);
-  res.status(StatusCodes.OK).json({
-    id: _id,
-    name,
-    email,
-  });
+const getMe = async(req, res) => {
+    const { _id, name, email } = await User.findById(req.user.id);
+    res.status(StatusCodes.OK).json({
+        id: _id,
+        name,
+        email,
+    });
 };
 
 //Generate JWT
 
 const generateToken = (payload) => {
-  return jwt.sign(
-    { id: payload.id, name: payload.name },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: "30d",
-    }
-  );
+    return jwt.sign({ id: payload.id, name: payload.name },
+        process.env.JWT_SECRET, {
+            expiresIn: "30d",
+        }
+    );
 };
 
 export { registerUser, loginUser, getMe };
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,12 @@
 import { useState, useEffect } from "react";
 import React from "react";
 import { FaSignInAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import { login, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
 
 function Login() {
   const [formdata, setFormdata] = useState({
@@ -10,6 +16,24 @@ function Login() {
 
   const { email, password } = formdata;
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
   const onChange = (e) => {
     setFormdata((prevState) => ({
       ...prevState,
@@ -18,7 +42,18 @@ function Login() {
   };
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData));
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
